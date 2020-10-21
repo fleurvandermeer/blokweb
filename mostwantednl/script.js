ddc0712a9bcbf680318beec932715042d1e1483e
fleurvandermeer <70395147+fleurvandermeer@users.noreply.github.com>
2020-10-21 17:11:58 +0200

    Add files via upload

---
+++ mostwantednl/script.js
@@ -0,0 +1,9 @@
+setTimeout( 
+    function startTimer(){
+        var tekstje = document.querySelector("main section:first-of-type p span"); 
+                
+        tekstje.innerHTML = 'ELKE WEEK NIEUWE ITEMS ONLINE';
+
+    }, 
+        3000
+); 
